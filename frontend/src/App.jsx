d4120635fb080280979abae6710c3e49f3e62297
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-01-07 16:18:59 +0400

    Header component create with responsive mobile burgerbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,10 @@
 import './App.css'
-import { Link, Routes, Route } from 'react-router-dom'
-import ListUser from './components/ListUser'
-import CreateUser from './components/CreateUser'
-import EditUser from './components/EditUser'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-      <h1>React CRUD App Operations using PHP API and MySql </h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to={"/"}>List User</Link>
-          </li>
-          <li>
-            <Link to={"user/create"}>Create User</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route index element={<ListUser />} />
-        <Route path="user/create" element={<CreateUser />} />
-        <Route path="user/:id/edit" element={<EditUser />} />
-      </Routes>
+      <Header />
     </>
   )
 }
